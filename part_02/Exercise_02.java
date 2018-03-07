ff451c06b3ec9feaa679492e2323ac354d2d0251
Danica <danicamaypriest@hotmail.co.uk>
2018-03-07 12:52:38 +0700

    merge

--- part_02/Exercise_02.java
+++ part_02/Exercise_02.java
@@ -14,6 +14,19 @@ import java.util.Scanner;
  */
 
 public class Exercise_02 {
+    public static void main(String[] args) {
+
+        double r = 3.14;
+        double h = 5;
+        double rsq = r * r;
+        double a = (2 * Math.PI * r * h) + (2 * Math.PI * rsq);
+        double pr =  2 * (r + r) + 2 * h;
+        System.out.println("The area of the cylinder is " + a);
+        System.out.println("The perimeter of the cylinder is " + pr);
+
+
+    }
+
 
 
 }
