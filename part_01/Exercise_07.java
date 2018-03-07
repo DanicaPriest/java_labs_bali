5f243a29e781fa97fbc5cda01fd05291c564f710
Danica <danicamaypriest@hotmail.co.uk>
2018-03-07 11:19:07 +0700

    adding part 2 ex 1

--- part_01/Exercise_07.java
+++ part_01/Exercise_07.java
@@ -12,7 +12,8 @@ package part_01;
 
 public class Exercise_07 {
     public static void main(String[] args) {
-double a = 1/3;
+        double answer = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+/*double a = 1/3;
 double b = 1/5;
 double c = 1/7;
 double d = 1/9;
@@ -20,7 +21,7 @@ double e = 1.0/11;
 double f = a + b;
 double g = c + d;
 double h = 1 - f - g - e;
-double answer = 4.0 * h;
+double answer = 4.0 * h;*/
         System.out.println(answer);
     }
 
