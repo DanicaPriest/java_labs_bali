6c9cb548cba2f5875429e19c765dbd8d61b65b50
student_guest <student_guest@Cadens-MacBook-Pro.local>
2018-03-07 13:35:14 +0800

    adding ex 5

--- part_02/Exercise_05.java
+++ part_02/Exercise_05.java
@@ -13,7 +13,30 @@ import java.util.Scanner;
  */
 public class Exercise_05 {
     public static void main(String[] args) {
+        System.out.println("Enter number:");
+        Scanner scanner = new Scanner(System.in);
+        int usernum = scanner.nextInt();
+        if (usernum == 1) {
+            System.out.println("ONE");
+        }
+        if (usernum == 2) {
+            System.out.println("TWO");
+        }if (usernum == 3) {
+            System.out.println("THREE");
+        }if (usernum == 4) {
+            System.out.println("FOUR");
+        }if (usernum == 5) {
+            System.out.println("FIVE");
+        }if (usernum == 6) {
+            System.out.println("SIX");
+        }if (usernum == 7) {
+            System.out.println("SEVEN");
+        }if (usernum == 8) {
+            System.out.println("EIGHT");
+        }if (usernum == 9) {
+            System.out.println("NINE");
 
+        }
     }
 
 }
