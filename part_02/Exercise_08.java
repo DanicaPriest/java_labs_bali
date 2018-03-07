2e70752a7928d39baba7b84a429e08076f7f3058
student_guest <student_guest@Cadens-MacBook-Pro.local>
2018-03-07 14:20:47 +0800

    ex 8

--- part_02/Exercise_08.java
+++ part_02/Exercise_08.java
@@ -12,11 +12,23 @@ import java.util.Scanner;
  */
 public class Exercise_08 {
     public static void main(String[] args) {
-        System.out.println("Enter number:");
+        System.out.println("Enter a number between 0 and 1,000,000,000:");
         Scanner scanner = new Scanner(System.in);
-        int val = scanner.nextInt();
+        int usernum = scanner.nextInt();
+        int max = 1000000000;
+        int i = 0;
+        while (i < max) {
+            if (i == usernum) {
+                System.out.println(i);
+                break;
+            }
+            else {
+                i++;
+            }
+
+        }
+
 
     }
-
 }
 
