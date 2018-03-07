5fffb5ed4088f18d6b5c69ab4b6b30b301cfbbb3
student_guest <student_guest@Cadens-MacBook-Pro.local>
2018-03-07 15:02:10 +0800

    ex 7

--- part_02/Exercise_07.java
+++ part_02/Exercise_07.java
@@ -14,6 +14,15 @@ import java.util.Scanner;
  */
 public class Exercise_07 {
     public static void main(String[] args) {
+        System.out.println("Enter a number in minutes between 0 and 1,000,000,000:");
+        Scanner scanner = new Scanner(System.in);
+        int minutes = scanner.nextInt();
+        int years = minutes/525600;
+        int daysleft = minutes % 525600;
+        int days = daysleft/1440;
+        
+
+        System.out.println(minutes + " minutes is " + years + " years and " + days + " days");
 
     }
 
