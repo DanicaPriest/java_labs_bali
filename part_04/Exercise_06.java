8394620bbe25bd5d4f520b975242761f6077f20a
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-08 12:40:11 +0700

    p4 e6

--- part_04/Exercise_06.java
+++ part_04/Exercise_06.java
@@ -11,3 +11,46 @@ package part_04;
  expected output: 0,2,4,6,8.....100
 
  */
+class Exercise_06 {
+
+    static class Queue {
+        private int q[];
+        private int putloc, getloc;
+
+        Queue(int size) {
+            q = new int[size];
+            putloc = getloc = 0;
+        }
+
+        void put(int num) {
+            if (putloc == q.length) {
+                return;
+            }
+            q[putloc++] = num;
+        }
+
+        int get() {
+            if (getloc == putloc) {
+                return 0;
+            }
+            return q[getloc++];
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Queue firstQ = new Queue(101);
+        int q;
+        int i;
+        for (i = 0; i < 101; i++){
+            firstQ.put(i);
+        }
+        for (int j = 0; j < 101; j ++){
+            q = firstQ.get();
+            if (q % 2 == 0)
+            {System.out.println(q);}
+
+        }
+    }
+}
