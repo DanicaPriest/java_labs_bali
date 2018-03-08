ccea8e4ae83b1e14c8926e263cb39ebf0aa2272c
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-08 13:32:26 +0700

    part 4 ex7

--- part_04/Exercise_07.java
+++ part_04/Exercise_07.java
@@ -11,3 +11,49 @@ The first plate put on the table is the last plate used. Create a stack class ca
 the method that access the stack push() and pop(). Allow the user to specify the size of the stack when it is created.
 Keep all other members of the Stack class private.
  */
+class Exercise_07 {
+    static class Stack{
+        private char s[];
+        private int putloc;
+
+        Stack(int size){
+            s = new char[size];
+            putloc = s.length -1;
+
+        }
+        void push(char ch){
+            if (putloc == 0){
+                return;
+            }
+            s[putloc--] = ch;
+
+        }
+        char pop(){
+            if (putloc == s.length){
+                return (char) 0;
+            }
+            return s[putloc++];
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Specify size of stack:");
+        Scanner scanner = new Scanner(System.in);
+        int sSize = scanner.nextInt();
+        Stack pancakes = new Stack(sSize);
+        int i;
+        char bananas;
+
+        for (i = 0; i < sSize; i++) {
+            pancakes.push((char) i);
+        }
+        for (int j = 0; j < sSize; j++) {
+            bananas = pancakes.pop();
+            System.out.print(bananas);
+            {
+
+            }
+        }
+    }
+
+}
