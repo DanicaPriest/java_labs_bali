f8a58db8ea30c919cf8acea24e1121d6d42947bb
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-08 10:13:56 +0700

    part 4 ex4

--- part_04/Exercise_04.java
+++ part_04/Exercise_04.java
@@ -13,12 +13,37 @@ class Exercise_04 {
                 {18, -456, 14, 6, 48, -80}
         };
         // uncomment the lines below when your methods are ready
-//        System.out.println(getMaxVal(dataArray));
-//        System.out.println(getMinVal(dataArray));
+        System.out.println(getMaxVal(dataArray));
+        System.out.println(getMinVal(dataArray));
     }
 
     // write a method called getMaxVal that takes in a 2D array and iterates through the array to determine the max value in array
+    public static int getMaxVal(int[][] arr) {
+        int maxNum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] >= maxNum) {
+                    maxNum = arr[i][j];
+
+                }
+
+            }
+        }
+        return maxNum;
+    }
 
     // write a method called getMinVal that takes in a 2D array and iterates through the array to determine the min value in array
+    public static int getMinVal(int[][] arr) {
+        int minNum = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] <= minNum) {
+                    minNum = arr[i][j];
 
+                }
+
+            }
+        }
+        return minNum;
+    }
 }
