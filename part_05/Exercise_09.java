6c1af3cf2c8aec08314976b0b42be07f194a1844
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-09 15:08:25 +0800

    ex 9

--- part_05/Exercise_09.java
+++ part_05/Exercise_09.java
@@ -1,11 +1,25 @@
 package part_05;
 
 /**
- * Demonstrate how variable-arity works, aka varargs. Create a method that uses "String ... varargs". Within the
+ * Demonstrate how variable-arity works, aka varargs. Create a method that uses "String ... varargs".
+ * Within the
  * varargs method, use StringBuilder to build a nicely formatted String/sentence using the varargs. Print this statement
  * to the console. Do this at least 3 times with different Strings/sentences.
  */
 
 
+class Exercise_09{
+    public static void main(String[] args) {
 
+    }
+
+
+
+    public void varagsMethod(String... varags){
+        StringBuilder ex9 = new StringBuilder();
+        ex9.append("This is");
+
+    }
+
+}
 
