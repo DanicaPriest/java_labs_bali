cbeae2c40bb46f1fed5fbd1e064ffef111527efa
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-09 17:04:51 +0800

    it's friday!

--- part_05/Exercise_09.java
+++ part_05/Exercise_09.java
@@ -8,18 +8,23 @@ package part_05;
  */
 
 
-class Exercise_09{
+class Exercise_09 {
     public static void main(String[] args) {
-
+        Exercise_09 test1 = new Exercise_09();
+        test1.varagsMethod("This", " is ", " a sentence!");
+        test1.varagsMethod("words", " words ", "and more words!");
+        test1.varagsMethod("so many words! ", " the words keep coming. ", " when will they stop? ", "only java knows", " maybe never.", " never ending words");
     }
 
 
-
-    public void varagsMethod(String... varags){
+    public void varagsMethod(String... varags) {
         StringBuilder ex9 = new StringBuilder();
-        ex9.append("This is");
+        for (int i = 0; i < varags.length; i++) {
+            ex9.append(varags[i]);
 
+        }
+        System.out.print(ex9.toString());
+        System.out.println();
     }
-
 }
 
