60c8da75c1b4ed351a9a75e779f18bd20c86f43d
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-13 10:18:13 +0800

    p9 ex 3

---
+++ part_09/Exercise_01_part4.java
@@ -0,0 +1,4 @@
+package part_09;
+
+public class Exercise_01_part4 {
+}
--- part_09/Exercise_03.java
+++ part_09/Exercise_03.java
@@ -1,16 +1,56 @@
 package part_09;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
 
 // Demonstrate how to read file using BufferedReader - demonstrate both the read() and readLine() methods()
 //be sure to close all connections
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class Exercise_03 {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 
 
-    }
+        String randomFile = "/home/danica/Documents/CodingNomads/randomwords.txt";
+        int i;
+        String str;
+
+        try (BufferedReader buff = new BufferedReader(new FileReader(randomFile))) {
+            System.out.println("This is the read() method:");
+            do {
+                i = buff.read();
+                System.out.print((char)i);
+            }
+            while(i != -1);
+            System.out.println();
+
+
+    } catch (IOException exc){
+            exc.printStackTrace();}
+
+           readLineMethod();
+        }
+    public static void readLineMethod() throws IOException{
+        String randomFile = "/home/danica/Documents/CodingNomads/randomwords.txt";
+        int i;
+        String str;
+
+        try (BufferedReader buff = new BufferedReader(new FileReader(randomFile))) {
+            System.out.println("This is the readLine() method:");
+
+        do {
+            str = buff.readLine();
+            System.out.print(str);
+        }
+        while((buff.readLine()) != null);
+    }catch (IOException exc){
+
+            exc.printStackTrace();}
+
+             }
 }
