e10fe57d59dc8b633c5a47f6bb6ac584f1e7e749
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-15 16:10:28 +0800

    stuff

--- part_11/Exercise_02.java
+++ part_11/Exercise_02.java
@@ -1,10 +1,27 @@
 package part_11;
 
+import java.util.Arrays;
+
 /**
  Write a generic method to exchange the positions of two different elements in an array.
  */
 
 public class Exercise_02 {
+    public static <E> void exchangeStuff(E[] array, int thing1, int thing2){
+      E temp = array[thing1];
+      array[thing1] = array[thing2];
+      array[thing2] = temp;
+    }
+    public static void main(String[] args) {
+        Integer numArr[] = {1, 2, 4, 5};
+        String sArr[] = {"Unicorn", "Rainbow"};
+        System.out.println(Arrays.toString(numArr));
+        System.out.println(Arrays.toString(sArr));
+        exchangeStuff(numArr, 0, 2);
+        exchangeStuff(sArr, 0, 1);
+        System.out.println(Arrays.toString(numArr));
+        System.out.println(Arrays.toString(sArr));
+    }
     
 }
 
--- part_11/Exercise_03.java
+++ part_11/Exercise_03.java
@@ -1,7 +1,38 @@
 package part_11;
 
+import part_08.Exercise_02;
+
 /**
  * Modify your Queue Class so that it is completely generic
  */
 public class Exercise_03 {
-}
+    public class Queue<E> {
+
+
+        private E[] q;
+        private int putloc, getloc;
+
+        public Queue(E[] array) {
+           q = array;
+            putloc = getloc = 0;
+        }
+
+        public synchronized void put(E num) {
+            if (putloc == q.length) {
+                return;
+            }
+            q[putloc++] = num;
+        }
+
+        public synchronized E get() throws Exercise_02.StackFull{
+            if (getloc == putloc) {
+                throw new Exercise_02.StackFull();
+            }
+            return q[getloc++];
+        }
+
+
+        }
+
+
+        }
