6cb31190ba756f5e6caf61cda0af1116d405788f
Danica <Danicamaypriest@hotmail.co.uk>
2018-03-16 11:36:58 +0800

    IOchallenge done

--- IOChallenge.java
+++ IOChallenge.java
@@ -11,27 +11,109 @@ challenge:
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class IOChallenge {
     public static void main(String[] args) {
 
 
         String path = "/home/danica/Documents/refugees_per_capita.csv";
-        String str;
 
-        try (BufferedReader buff = new BufferedReader(new FileReader(path))) {
+            //reading file with Buffered reader
+        try (FileReader fr = new FileReader(path); BufferedReader buff = new BufferedReader(fr)) {
+            ArrayList<Refugees> refugeedata = new ArrayList();
+
+            String str;
+
+            //Reading data, putting it into an array and mapping data to Refugees object
+            while ((str = buff.readLine()) != null) {
+                String data[] = str.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                Refugees ref = new Refugees();
+                try {
+                    ref.setYear(Integer.parseInt(data[1]));
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+                ref.setCountry(data[2]);
+
+                try {
+                    ref.setPopulation(Double.parseDouble(data[3]));
 
 
-            do {
-                str = buff.readLine();
-                System.out.println(str);
+                } catch (NumberFormatException e) {
+                    refugeedata.add(ref);
+
+                    continue;
+                }
+
+                try {
+                    ref.setRefugees(Double.parseDouble(data[4]));
+
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    refugeedata.add(ref);
+                    continue;
+                }
+                ref.setRefPerCap(Double.parseDouble(data[5]));
+                refugeedata.add(ref);
+
+
             }
-            while ((buff.readLine()) != null);
+            //calculate total population and refugees
+            double totalPopulation = 0.0;
+            double totalRefuees = 0.0;
+            for (Refugees r : refugeedata) {
+
+                totalPopulation += r.getPopulation();
+                totalRefuees += r.getRefugees();
+
+
+            }
+            System.out.println("Total Population: " + totalPopulation);
+            System.out.println("Total Refugees: " + totalRefuees);
+            //calculate ratio of refugees to population
+
+            double ratio = totalPopulation / totalRefuees;
+            System.out.println("The ratio of total refugees to total population is 1 to " + ratio);
+
+            //call data per year method for each year
+            System.out.println("Data breakdown per year:");
+            dataperYear(2001, refugeedata);
+            dataperYear(2002, refugeedata);
+            dataperYear(2003, refugeedata);
+            dataperYear(2004, refugeedata);
+            dataperYear(2005, refugeedata);
+            dataperYear(2006, refugeedata);
+            dataperYear(2007, refugeedata);
+            dataperYear(2008, refugeedata);
+            dataperYear(2009, refugeedata);
+            dataperYear(2010, refugeedata);
+            dataperYear(2011, refugeedata);
+            dataperYear(2012, refugeedata);
+            dataperYear(2013, refugeedata);
+            dataperYear(2014, refugeedata);
+            dataperYear(2015, refugeedata);
+
+
+
         } catch (IOException exc) {
 
             exc.printStackTrace();
         }
 
-    }
 
+    }
+    //method to divide data into years
+    public static void dataperYear(int year, ArrayList<Refugees> arr){
+        double totalPopperYear = 0.0;
+        double totalRefperYear = 0.0;
+        double ratioperYear;
+        for (Refugees r : arr) {
+            if (r.getYear() == year) {
+                totalPopperYear += r.getPopulation();
+                totalRefperYear += r.getRefugees();
+            }
+        }
+        ratioperYear = totalPopperYear / totalRefperYear;
+        System.out.println("Total population for " + year + ": " + totalPopperYear + ", total refugees: " + totalRefperYear + ", ratio of refugees to population: 1 to " + ratioperYear);
+    }
 }
--- Refugees.java
+++ Refugees.java
@@ -1,2 +1,65 @@
 public class Refugees {
+int Year;
+String Country;
+double Population;
+double Refugees;
+double RefPerCap;
+double TotalPopulation;
+double TotalRefugees;
+
+    public double getTotalRefugees() {
+        return TotalRefugees;
+    }
+
+    public void setTotalRefugees(double totalRefugees) {
+        TotalRefugees = totalRefugees;
+    }
+
+    public double getTotalPopulation() {
+        return TotalPopulation;
+    }
+
+    public void setTotalPopulation(double totalPopulation) {
+        TotalPopulation = totalPopulation;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int year) {
+        Year = year;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public double getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(double population) {
+        Population = population;
+    }
+
+    public double getRefugees() {
+        return Refugees;
+    }
+
+    public void setRefugees(double refugees) {
+        Refugees = refugees;
+    }
+
+    public double getRefPerCap() {
+        return RefPerCap;
+    }
+
+    public void setRefPerCap(double refPerCap) {
+        RefPerCap = refPerCap;
+    }
 }
